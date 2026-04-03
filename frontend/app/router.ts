21c38fa01ab8f6e51a78338339ee9e37f866ab15
RubenPari <rubenpari.developer@gmail.com>
2026-04-04 01:41:04 +0200

    chore(frontend): add app core structure

---
+++ frontend/app/router.ts
@@ -0,0 +1,18 @@
+import EmberRouter from '@embroider/router';
+import config from 'frontend/config/environment';
+
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
+
+Router.map(function () {
+  this.route('random', { path: '/' });
+  this.route('history');
+  this.route('vault');
+  this.route('stats');
+  this.route('login');
+  this.route('register');
+  this.route('reset');
+  this.route('settings');
+});
